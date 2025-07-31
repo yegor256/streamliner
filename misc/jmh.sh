fde719d001629e8da5806e7d932d73b0338b59a6
Yegor Bugayenko <yegor256@gmail.com>
2025-07-31 07:44:01 +0300

    self path

--- misc/jmh.sh
+++ misc/jmh.sh
@@ -2,13 +2,7 @@
 
 set -euo pipefail
 
-if [ ! -f "pom.xml" ]; then
-  cd ..
-  if [ ! -f "pom.xml" ]; then
-    echo "Run jmh.sh from project or misc directory"
-    exit 1
-  fi
-fi
+self=$(dirname "$0")
 
 if [ "$#" -eq 0 ]; then
   ARGS="dk.casa.streamliner.jmh.Test.*"
@@ -16,7 +10,7 @@ else
   ARGS="$@"
 fi
 
-mvn compile
+mvn --file "${self}/../pom.xml" compile
 
 packages=(
   java.util
@@ -31,8 +25,8 @@ for p in "${packages[@]}"; do
 done
 export "MAVEN_OPTS=${opts[*]}"
 
-mvn exec:java -Dexec.mainClass=dk.casa.streamliner.asm.TransformASM
+mvn --file "${self}/../pom.xml" exec:java -Dexec.mainClass=dk.casa.streamliner.asm.TransformASM
 
-CP=$(mvn -q exec:exec -Dexec.executable=echo -Dexec.args="%classpath" 2> /dev/null)
+CP=$(mvn -q --file "${self}/../pom.xml" exec:exec -Dexec.executable=echo -Dexec.args="%classpath" 2> /dev/null)
 
-java -Dfile.encoding=UTF-8 -classpath out/asm/:$CP org.openjdk.jmh.Main -rf JSON -rff misc/out.json $ARGS
+java -Dfile.encoding=UTF-8 -classpath "${self}/out/asm/:$CP" org.openjdk.jmh.Main -rf JSON -rff --file "${self}/out.json" $ARGS
